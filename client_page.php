d7ef9e1cb38bd56095ec351a8855c5b384e0761f
tairmiron <tairmiron@gmail.com>
2019-03-23 16:22:58 +0200

    upload client_page.php file - add code and fix commit

--- client_page.php
+++ client_page.php
@@ -17,7 +17,49 @@
     <h1 id="white">My Page</h1>      
   </div>
   <div>
-
+<?php
+ if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    echo 'Loading page - GET';
+    echo " ";
+  }
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    echo 'Submit was clicked - POST';
+    echo " ";
+    $host = 'localhost';     
+    $user = 'root';
+    $password = '';
+    $pass='0';
+    $dataBaseName = 'samim';
+    $id= $_POST['id'];
+    $flag=0;
+    $password= $_POST['password'];
+    $message_password = "wrong password";
+    $message_id = "ID does not exist";
+    $mysqli = new mysqli($host,$user,'',$dataBaseName);
+    $sql = "SELECT * FROM clients";
+    $result = $mysqli->query($sql);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            if ($row["c_id"]==$id){
+                  $flag = $flag + 1;
+                  if ($password != $row['c_password1']) {
+                    echo "<script type='text/javascript'>alert('$message_password');</script>";
+                  }
+                  else{
+                    echo "<table style='margin-top: 2%; margin-bottom: 2%; text-align: center; color: white; border: 3px solid rgb(253, 253, 253);'><tr><th style='text-align: center;'>First Name</th><th style='text-align: center;'>Last Name</th><th style='text-align: center;'>Password</th><th style='text-align: center;'>ID</th><th style='text-align: center;'>Gender</th><th style='text-align: center;'>Phone</th><th style='text-align: center;'>Mail</th><th style='text-align: center;'>Country</th><th style='text-align: center;'>City</th><th style='text-align: center;'>Status</th><th style='text-align: center;'>Date</th><th style='text-align: center;'>My Story</th>";//<th style='text-align: center;'>Edit</th><th style='text-align: center;'>Remove</th></tr>;
+                    echo "<tr><td>" . $row["c_first_name"]. "</td><td>" . $row["c_last_name"]. "</td><td>" . $row["c_password1"]. "</td><td>" . $row["c_id"]. "</td><td>" . $row["c_gender"]. "</td><td>" . $row["c_phone"]. "</td><td>" . $row["c_mail"]. "</td><td>" . $row["c_country"]. "</td><td>" . $row["c_city"]. "</td><td>" . $row["c_status"]. "</td><td>" . $row["c_date"]. "</td><td>" . $row["c_story"];
+                    echo "</table>";
+                    //. "</td><td>'<a href = 'edit.php?id=$id'><i class='fa fa-edit' style='font-size:45px;color:black;'></i></a>'</td><td>'<a href = 'delete.php?id=$id'><i class='fa fa-trash-o' style='font-size:48px;color:black;'></i></a>'</td></tr>";
+                  }
+                }
+              }
+              if ($flag==0){
+                echo "<script type='text/javascript'>alert('$message_id');</script>";
+              }
+            }
+            $mysqli->close();
+    }
+?>
 </div>     
 </div>
 <a href = 'login_choose.html'><i style="font-size:45px; color:white; margin-top: 2%; margin-left:48%; margin-bottom: 2%;" class="fa fa-undo"></i></a>
